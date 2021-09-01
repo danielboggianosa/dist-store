567250203c052f8e8769ae3961f5f6874daf54e2
Daniel Boggiano Sáenz <dev@danielboggiano.com>
2021-09-01 16:50:43 -0500

    learn: use of angular schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,10 @@ const routes: Routes = [
       },
     ]
   },
-
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
   {
     path: '**',
     component: PageNotFoundComponent

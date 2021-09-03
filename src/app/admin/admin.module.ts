57d4ef27d9278aa6cd8e177518a91996f4653e58
Daniel Boggiano Sáenz <dev@danielboggiano.com>
2021-09-03 05:47:50 -0500

    chore: form products

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,10 +11,11 @@ import { NavComponent } from './components/nav/nav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { TableComponent } from './components/table/table.component';
+import { FormProductComponent } from './components/form-product/form-product.component';
 
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, TableComponent, DashboardComponent, ProductsListComponent],
+  declarations: [ProductFormComponent, NavComponent, TableComponent, DashboardComponent, ProductsListComponent, FormProductComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,

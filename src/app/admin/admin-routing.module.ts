23f5b062d5996189ff35a3c27e9a068ee4f05cf1
Daniel Boggiano Sáenz <dev@danielboggiano.com>
2021-09-02 23:53:27 -0500

    chore: Dashboard and inventory

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { NavComponent } from './components/nav/nav.component';
+import { Routes, RouterModule } from '@angular/router';
+
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { TableComponent } from './components/table/table.component';
 
 const routes: Routes = [
   {
@@ -11,9 +15,21 @@ const routes: Routes = [
       {
         path: 'create',
         component: ProductFormComponent
-      }
+      },
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'table',
+        component: TableComponent
+      },
+      {
+        path: 'products',
+        component: ProductsListComponent
+      },
     ]
-  },
+  }
 ];
 
 @NgModule({
